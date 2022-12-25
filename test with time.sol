184d1b802170bc0c5a177bab71a902076081818e
RexLeung0919 <leunglapmun@gmail.com>
2022-12-26 02:27:23 +0800

    Create test with time.sol

---
+++ test with time.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+// Our first contract is a faucet!
+contract Divide{
+    // Declaring the
+    // state variables
+    int256 No1 ;
+    int256 No2 ;
+    uint256 public createTime;
+
+    function Time() public {
+        createTime = block.timestamp;
+    }
+     
+    // Defining a function
+    // to set the value of
+    // the first variable
+    function setNo1(int128 x) public
+    {
+        No1 = x;
+    }
+     
+    // Defining function
+    // to set the value of
+    // the second variable
+    function setNo2(int128 y) public
+    {
+        No2 = y;
+    }
+     
+    // Defining function to
+    // return the result
+    function opDivide() view public returns (int256)
+    {
+        //require(secondNo > 0, "The second parameter should be larger than 0");
+        int256 answer = No1 / No2 ;
+        return answer;
+    }
+
+    
+    function TotalTime() view public returns (uint256) {
+        uint256 endtime = block.timestamp - createTime;
+        return endtime;
+    }
+
+}

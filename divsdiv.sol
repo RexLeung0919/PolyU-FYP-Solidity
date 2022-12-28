3700e463f54b7e891d653a97f35f8b7b12b27c19
RL <70059242+RexLeung0919@users.noreply.github.com>
2022-12-28 10:32:06 +0800

    Update divsdiv.sol

--- divsdiv.sol
+++ divsdiv.sol
@@ -1,2 +1,25 @@
-func op_Div(stack*Stack){
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// Our first contract is a faucet!
+contract Divide{
+
+  func op_Div(stack*Stack){
+    x,y:stack.pop(),stack.pop()
+    if y.Sign()!=0{
+      stack.push(math.U256(Div(x,y)))
+    } else {
+      stack.push(new(big.Int)) 
+    }
+  
+  
+  }
+  
+  func op_Sdiv(stack*Stack){
+  
+  
+  }
+  
+  
+  
 }

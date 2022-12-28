95738a284356818476569015630f2d9822e5221a
RL <70059242+RexLeung0919@users.noreply.github.com>
2022-12-28 16:46:20 +0800

    Update divsdiv.sol

--- divsdiv.sol
+++ divsdiv.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+big.Int// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 // Our first contract is a faucet!
@@ -11,15 +11,20 @@ contract Divide{
     } else {
       stack.push(new(big.Int)) 
     }
-  
-  
   }
   
   func op_Sdiv(stack*Stack){
-  
-  
+  x,y: = math.S256(stack.pop()),math.S256(stack.pop())
+  If y.Sign() == 0{
+    stack.push(new(big.Int))
+  } else {
+    if evm.interpreter.intPool.get().Mul(x,y).Sign() < 0 {
+    n.SetInt64(-1)
+  } else
+    n.SetInt64(1)
   }
-  
-  
-  
+  res:=x.Div(x.Abs(x).y.Abs(y))
+  res.Mul(res, n)
+  stack.push(math.U256(res))
+  }
 }
